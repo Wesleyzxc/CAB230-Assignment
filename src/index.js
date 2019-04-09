e7894ed17955c97e4fe07430f9e9caf2d14a2813
Wesley <wesleykok-@hotmail.com>
2019-04-10 09:42:48 +1000

    Login in button will generate a token

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+function LoginButton() {
+  const fetchToken = () => {
+    fetch("https://cab230.hackhouse.sh/login", {
+      method: "POST",
+      body: 'email=n9972676%40qut.edu.au&password=testaccount',
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded"
+      }
+    })
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok");
+      })
+      .then(function (result) {
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.log("There has been a problem with your fetch operation: ", error.message);
+      });
+  }
+  return (
+    <div>
+      <p>Crime statistics</p>
+      <button onClick={fetchToken}>Login!</button>
+    </div >
+  )
+}
+
 function LikeCounter() {
   const [count, setCount] = useState(0);
   const [superLike, setSuperCount] = useState(0);
@@ -52,8 +82,9 @@ function App() {
     // how to get padding
     <div className="App">
       {headlines.map(headline => (
-        <Headline key={headline.key} title={headline.title} />
+        <Headline key={headline.url} title={headline.title} />
       ))}
+      <LoginButton />
     </div>
   );
 }

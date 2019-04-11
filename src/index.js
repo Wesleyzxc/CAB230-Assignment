48c6ec1e8249d6a35c6169c3a218b1ffcf01ee35
Wesley <wesleykok-@hotmail.com>
2019-04-11 17:26:02 +1000

    new offences hitting in a loop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+import "./index.css";
+
+function RegisterButton(){
+  var returnedMessage
+   const fetchToken = () => {
+    fetch("https://cab230.hackhouse.sh/register", {
+      method: "POST",
+      body: 'email=n9972676%40qut.edu.au&password=testaccount',
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded"
+      }
+    })
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok");
+      })
+      .then(function (result) {
+        console.log(result);
+        ;
+      })
+      .catch(function (error) {
+        returnedMessage = error.message;
+        console.log(returnedMessage);
+        console.log("There has been a problem with registering. Are you already registered? ", error.message);
+      });
+  }
+  return (
+    <div>
+      <p>Register your account {returnedMessage}</p>
+      <button onClick={fetchToken}>Register!</button>
+      
+    </div >
+  )
+}
+
 
 function LoginButton() {
   const fetchToken = () => {
@@ -27,63 +63,60 @@ function LoginButton() {
   }
   return (
     <div>
-      <p>Crime statistics</p>
-      <button onClick={fetchToken}>Login!</button>
+      <p>Login details</p>
+      <button onClick={fetchToken}>Login</button>
     </div >
   )
 }
 
-function LikeCounter() {
-  const [count, setCount] = useState(0);
-  const [superLike, setSuperCount] = useState(0);
-  const increment = () => {
-    setCount(oldCount => oldCount + 1);
-  };
+function getOffences() {
+  return fetch("https://cab230.hackhouse.sh/offences")
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok");
+      })
+      .catch(function (error) {
+        console.log("There has been a problem with your fetch operation: ", error.message);
+      });
+  }
 
-  const decrement = () => {
-    if (count > 0) setCount(oldCount => oldCount - 1);
-  };
+  function useOffences(){
+    const [loading, setLoading] = useState(true);
+    const [offences, setOffences] = useState();
+    const [error, setError] = useState(null);
 
-  const superLikes = () => {
-    if (superLike < 2) {
-      setCount(oldCount => oldCount + 10);
-      setSuperCount(superLike => superLike + 1);
-    }
-  };
-  return (
-    // how to get padding
-    <div>
-      <p>Like Count: {count}</p>
-      <button onClick={increment}>Like</button>
-      <button onClick={superLikes}>Superlike</button>
-      <button onClick={decrement}>Dislike</button>
-    </div>
-  );
-}
+    useEffect(() => {
+      getOffences()
+        .then(offences => {
+          setOffences(offences);
+          setLoading(false);
+        })
+        .catch(e => {
+          setError(e);
+          setLoading(false);
+        });
+    });
+  
+    return {
+      loading,
+      offences,
+      error: null
+    };
+  }
 
-function Headline(props) {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      <LikeCounter />
-    </div>
-  );
-}
 
 function App() {
-  const headlines = [
-    { title: "My First Title", url: "https://news.com/first-title" },
-    { title: "My Second Title", url: "https://news.com/second-title" },
-    { title: "My Third Title", url: "https://news.com/third-title" },
-    { title: "My Fourth Title", url: "https://news.com/fourth-title" }
-  ];
+  const { loading, offences, error} = useOffences();
+  if (loading) {
+    return <p>Loading...</p>
+  }
 
   return (
     // how to get padding
-    <div className="App">
-      {headlines.map(headline => (
-        <Headline key={headline.url} title={headline.title} />
-      ))}
+    <div className="Credentials">
+      <RegisterButton />
       <LoginButton />
     </div>
   );
